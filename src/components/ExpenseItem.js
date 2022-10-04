0c69c7afbbfe53dacc6e57de5432bf2fee15c7ff
Sangkun-svg <gims4752@gmail.com>
2022-10-04 15:37:50 +0900

    lecture 3-11

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+export const ExpenseItem = () => {
+  return (
+    <div>
+      <div>2021.03.28</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$ 294.67</div>
+      </div>
+    </div>
+  );
+};
