a18c90c556315e3bb86850a630f6f8342f180f00
Sangkun-svg <gims4752@gmail.com>
2022-10-07 15:41:18 +0900

    lecture : 4-14 ~ 4-20

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,19 +1,33 @@
 import "./Expenses.css";
 import { ExpenseItem } from "./ExpenseItem";
 import { Card } from "../common/Card";
+import { ExpensesFilter } from "../ExpensesFilter";
+import { useState } from "react";
+
 export const Expenses = ({ expense }) => {
+  const [filteredYear, setFilteredYear] = useState("2019");
+  const filterChangeHadnler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {expense.map((el) => {
-        return (
-          <ExpenseItem
-            key={el.id}
-            title={el.title}
-            amount={el.amount}
-            date={el.date}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHadnler}
+        />
+        {expense.map((el) => {
+          return (
+            <ExpenseItem
+              key={el.id}
+              title={el.title}
+              amount={el.amount}
+              date={el.date}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 };
